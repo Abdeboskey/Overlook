c66b72363ecd7cd4998b30af5b85915a54f1b44b
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-30 15:20:02 -0600

    Create booking id and user id tests #13

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -20,5 +20,29 @@ describe.only('Booking', () => {
     roomServiceCharges: [],
   }
 
+  it('should be a function', () => {
+    expect(Booking).to.be.a('function')
+  })
 
+  it('should be an instance of Booking', () => {
+    const reservation = new Booking(booking)
+    
+    expect(reservation).to.be.an.instanceof(Booking)
+  })
+  
+  it('should have a booking id', () => {
+    const reservation = new Booking(booking)
+    const anotherReservation = new Booking(booking2)
+
+    expect(reservation.id).to.equal("5fwrgu4i7k55hl6sz")
+    expect(anotherReservation.id).to.equal("5fwrgu4i7k55hl6t5")
+  })
+
+  it('should have a user id', () => {
+    const reservation = new Booking(booking)
+    const anotherReservation = new Booking(booking2)
+
+    expect(reservation.userId).to.equal(9)
+    expect(anotherReservation.userId).to.equal(43)
+  })
 })
