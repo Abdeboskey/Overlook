f32c94e3a5a0eee139312ca102c75d5b796ef395
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-30 16:33:42 -0600

    Create room service methods and tests, pass tests #13

--- src/Booking.js
+++ src/Booking.js
@@ -2,6 +2,20 @@ class Booking {
   constructor(booking) {
     this.id = booking.id
     this.userId = booking.userID
+    this.date = booking.date
+    this.roomNumber = booking.roomNumber
+    this.roomServiceCharges = []
+  }
+
+  orderRoomService(menuItem) {
+    this.roomServiceCharges.push(menuItem.price)
+  }
+
+  getRoomServiceBill() {
+    return this.roomServiceCharges.reduce((total, charge) => {
+      total += charge
+      return total
+    }, 0)
   }
 }
 
--- test/Booking-test.js
+++ test/Booking-test.js
@@ -9,7 +9,7 @@ describe.only('Booking', () => {
     userID: 9,
     date: "2020/04/22",
     roomNumber: 15,
-    roomServiceCharges: [],
+    roomServiceCharges: []
   }
 
   const booking2 = {
@@ -17,8 +17,26 @@ describe.only('Booking', () => {
     userID: 43,
     date: "2020/01/24",
     roomNumber: 24,
-    roomServiceCharges: [],
+    roomServiceCharges: []
   }
+
+  const bltSandwich = {
+    item: "BLT Sandwich",
+    price: 15.5
+  }
+
+  const chickenTenders = {
+    item: "Chicken Tenders",
+    price: 12.4
+  }
+
+  let reservation
+  let anotherReservation
+
+  beforeEach(() => {
+    reservation = new Booking(booking)
+    anotherReservation = new Booking(booking2)
+  })
 
   it('should be a function', () => {
     expect(Booking).to.be.a('function')
@@ -31,18 +49,39 @@ describe.only('Booking', () => {
   })
   
   it('should have a booking id', () => {
-    const reservation = new Booking(booking)
-    const anotherReservation = new Booking(booking2)
-
     expect(reservation.id).to.equal("5fwrgu4i7k55hl6sz")
     expect(anotherReservation.id).to.equal("5fwrgu4i7k55hl6t5")
   })
 
-  it('should have a user id', () => {
-    const reservation = new Booking(booking)
-    const anotherReservation = new Booking(booking2)
-
+  it('should have a user id', () => {    
     expect(reservation.userId).to.equal(9)
     expect(anotherReservation.userId).to.equal(43)
+  })
+  
+  it('should have a booking date', () => {
+    expect(reservation.date).to.equal("2020/04/22")
+    expect(anotherReservation.date).to.equal("2020/01/24")
+  })
+  
+  it('should have a room number', () => {
+    expect(reservation.roomNumber).to.equal(15)
+    expect(anotherReservation.roomNumber).to.equal(24)
+  })
+  
+  it('should be able to keep track of room service charges', () => {
+    expect(reservation.roomServiceCharges).to.deep.equal([])
+  })
+
+  it('should be able to add room service charges', () => {
+    reservation.orderRoomService(chickenTenders)
+    
+    expect(reservation.roomServiceCharges[0]).to.equal(12.4)
+  })
+  
+  it('should be able to get the total amount of room service charges made during a booking', () => {
+    reservation.orderRoomService(bltSandwich)
+    reservation.orderRoomService(chickenTenders)
+    
+    expect(reservation.getRoomServiceBill()).to.equal(27.9)
   })
 })
