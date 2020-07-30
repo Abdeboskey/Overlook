b437cb94d5f9c3670dd05c28c453733a2b62661f
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-30 15:10:17 -0600

    Create booking test imports and initial setup #13

---
+++ test/Booking-test.js
@@ -0,0 +1,24 @@
+import chai from "chai"
+import Booking from "../src/Booking"
+const expect = chai.expect
+
+describe.only('Booking', () => {
+  
+  const booking = {
+    id: "5fwrgu4i7k55hl6sz",
+    userID: 9,
+    date: "2020/04/22",
+    roomNumber: 15,
+    roomServiceCharges: [],
+  }
+
+  const booking2 = {
+    id: "5fwrgu4i7k55hl6t5",
+    userID: 43,
+    date: "2020/01/24",
+    roomNumber: 24,
+    roomServiceCharges: [],
+  }
+
+
+})
