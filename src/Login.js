fc0a0c2d035e074e0b6ffa9bf8d7bd64786e263d
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-30 23:01:42 -0600

    Create methods on login class #15

---
+++ src/Login.js
@@ -0,0 +1,39 @@
+class Login {
+  constructor(username, password) {
+    this.username = username
+    this.attemptedPassword = password
+    this.correctPassword = 'overlook2020'
+  }
+
+  checkPassword() {
+    return this.attemptedPassword === this.correctPassword
+  }
+
+  checkIfCustomer() {
+    let name = this.username.trim().slice(0, 8).toLowerCase()
+    return name === 'customer'
+  }
+  
+  validateCustomerId() {
+    let number = Number(this.username.trim().slice(-2));
+    return number > 0 && number <= 50;
+  }
+
+  checkIfManager() {
+    return this.username.trim().toLowerCase() === 'manager'
+  }
+
+  authenticateUser() {
+    if (this.checkIfManager() && this.checkPassword()) {
+      return 'manager'
+    } else if (this.checkIfCustomer() && this.validateCustomerId() && this.checkPassword()) {
+      return 'guest'
+    } else if ((!this.checkIfCustomer() || !this.validateCustomerId()) && !this.checkIfManager()) {
+      return 'Invalid Username. Please Try Again.'
+    } else if (!this.checkPassword()) {
+      return 'Incorrect Password. Please Try Again.'
+    }
+  }
+}
+
+export default Login
