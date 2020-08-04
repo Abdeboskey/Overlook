f13e11f4cfac79b0776b35d5c343c38d13f7775f
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-04 16:33:38 -0600

    Change all quotes to single quotes #36

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -2,26 +2,26 @@ class DomUpdates {
   constructor() {}
 
   showLoginError(usernameInput, passwordInput, loginResult) {
-    let invalidInfo = document.querySelector(".login-error-message");
-    usernameInput.value = ""; //dom
-    passwordInput.value = ""; // dom
-    invalidInfo.innerText = loginResult;
-    this.displayElement("login-error-message");
+    let invalidInfo = document.querySelector('.login-error-message')
+    usernameInput.value = ''
+    passwordInput.value = ''
+    invalidInfo.innerText = loginResult
+    this.displayElement('login-error-message')
   }
 
   formatDate(date) {
-    date = date.split("/");
-    date.push(date.shift());
-    if (date[0].charAt(0) === "0") date[0] = date[0].slice(1);
-    if (date[1].charAt(0) === "0") date[1] = date[1].slice(1);
-    return date.join("/");
+    date = date.split('/')
+    date.push(date.shift())
+    if (date[0].charAt(0) === '0') date[0] = date[0].slice(1)
+    if (date[1].charAt(0) === '0') date[1] = date[1].slice(1)
+    return date.join('/')
   }
 
-  updateGuestLog(currentGuestName, costofBookings) {
-    const guestName = document.querySelector(".guest-name");
-    const spendings = document.querySelector(".guest-spendings");
-    guestName.innerText = `Welcome, ${currentGuestName}`;
-    spendings.innerText = `Total spent at The Overlook: $${costofBookings}`;
+  updateGuestLog(currentGuestName, costOfBookings) {
+    const guestName = document.querySelector('.guest-name')
+    const spendings = document.querySelector('.guest-spendings')
+    guestName.innerText = `Welcome, ${currentGuestName}`
+    spendings.innerText = `Total spent at The Overlook: $${costOfBookings}`
   }
 
   updateManagersLog(
@@ -30,37 +30,37 @@ class DomUpdates {
     todaysTotalRevenue,
     currentDate
   ) {
-    const date = document.querySelector(".manager-date");
-    const revenue = document.querySelector(".revenue-today");
-    const occupied = document.querySelector(".percentage-occupied");
-    const available = document.querySelector(".rooms-available");
+    const date = document.querySelector('.manager-date')
+    const revenue = document.querySelector('.revenue-today')
+    const occupied = document.querySelector('.percentage-occupied')
+    const available = document.querySelector('.rooms-available')
     date.innerText = `• Manager's Log - Stardate ${this.formatDate(
       currentDate
-    )} •`;
-    revenue.innerText = `▶ Total Revenue Today: $${todaysTotalRevenue}`;
-    occupied.innerText = `▶ ${percentageOccupied}% of rooms are currently occupied`;
-    available.innerText = `▶ ${availableRooms} rooms are currently available`;
+    )} •`
+    revenue.innerText = `▶ Total Revenue Today: $${todaysTotalRevenue}`
+    occupied.innerText = `▶ ${percentageOccupied}% of rooms are currently occupied`
+    available.innerText = `▶ ${availableRooms} rooms are currently available`
   }
 
   showManagerDashboard(percentage, available, todaysRevenue, currentDate) {
-    this.updateManagersLog(percentage, available, todaysRevenue, currentDate);
-    this.hideElement("landing");
-    this.displayElement("manager-dashboard");
+    this.updateManagersLog(percentage, available, todaysRevenue, currentDate)
+    this.hideElement('landing')
+    this.displayElement('manager-dashboard')
   }
 
   showGuestDashboard(guestName, costOfBookings) {
     this.updateGuestLog(guestName, costOfBookings)
-    this.hideElement("landing")
-    this.displayElement("guest-dashboard")
+    this.hideElement('landing')
+    this.displayElement('guest-dashboard')
   }
 
   hideElement(className) {
-    document.querySelector(`.${className}`).classList.add("hidden");
+    document.querySelector(`.${className}`).classList.add('hidden')
   }
 
   displayElement(className) {
-    document.querySelector(`.${className}`).classList.remove("hidden");
+    document.querySelector(`.${className}`).classList.remove('hidden')
   }
 }
 
-export default DomUpdates;
+export default DomUpdates
