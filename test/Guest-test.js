98eaf1847b0a3a7d0c0118dd755cc7b5c500ef50
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-03 19:23:56 -0600

    Add before hook, and test for empty bookings array #26

--- test/Guest-test.js
+++ test/Guest-test.js
@@ -4,40 +4,42 @@ const expect = chai.expect
 
 describe.only('Guest', () => {
 
-  const user = {
-    id: 1,
-    name: "Max Headroom"
-  }
+  let user, newUser, guest, nuGuest
 
-  const newUser = {
-    id: 24,
-    name: "Gingerboat Forgotten"
-  };
+  before(() => {
+    user = {
+      id: 1,
+      name: "Max Headroom"
+    }
+  
+    newUser = {
+      id: 24,
+      name: "Gingerboat Forgotten"
+    };
+
+    guest = new Guest(user)
+    nuGuest = new Guest(newUser)
+  })
 
   it('should be a function', () => {
     expect(Guest).to.be.a('function')
   })
 
   it('should be an instance of Guest', () => {
-    const guest = new Guest(user)
     expect(guest).to.be.an.instanceof(Guest)
   })
 
   it('should have an ID number', () => {
-    const guest = new Guest(user)
     expect(guest.id).to.equal(1)
-    
-    const nuGuest = new Guest(newUser)
     expect(nuGuest.id).to.equal(24)
   })
   
   it('should have a name', () => {
-    const guest = new Guest(user)
     expect(guest.name).to.equal("Max Headroom");
-    
-    const nuGuest = new Guest(newUser)
     expect(nuGuest.name).to.equal("Gingerboat Forgotten");
   })
 
-  
+  it('should start with no bookings', () => {
+    expect(guest.bookings).to.eql([])
+  })
 })
